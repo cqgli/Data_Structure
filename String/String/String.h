683ac796249b29aa01cd81b52e6edd28982dd753
lizhenxin <2754502012@qq.com>
2025-04-14 21:11:22 +0800

    string

--- String/String/String.h
+++ String/String/String.h
@@ -45,5 +45,51 @@ int index(SString S, SString T);
 void Print_String(SString S);
 
 
+//朴素模式匹配算法
+int index(SString S, SString T)
+{
+	int i = 1, j = 1;
+	if (S.length < T.length || T.length == 0)
+		return -1;
+	while (i <= S.length && j <= T.length)
+	{
+		if (S.ch[i] == T.ch[j])
+		{
+			i++;
+			j++;
+		}
+		else
+		{
+			i = i - j + 2;
+			j = 1;
+		}
+	}
+	if (j > T.length)
+		return i - T.length;
+	return -1;
+}
+
+//KMP算法
+int KMP(SString S, SString T, int next[])
+{
+	int i = 1, j = 1;
+	while (i <= S.length && j <= T.length)
+	{
+		if (j == 0 || S.ch[i] == S.ch[j])
+		{
+			i++;
+			j++;
+		}
+		else
+		{
+			j = next[j];
+		}
+	}
+	if (j > T.length)
+		return i-T.length;
+	return -1;
+}
+
+
 
 
